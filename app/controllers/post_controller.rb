54985329d389c174cc4ab5e5c93024970e61c46c
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-04-22 15:06:28 +0200

    create and retrieve all records

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,8 @@
 class PostController < ApplicationController
-    def index
-        post = Post.create({title:"mohamed",desc:"I am a man called moAyman saleh mohamed"})
+    post = Post.create(title: "Mohamed", desc: "I am a man called MoAyman Saleh Mohamed")
+    all_posts = Post.all
+
+    all_posts.each do |post|
+      puts "Title: #{post.title}, Description: #{post.desc}"
     end
 end
