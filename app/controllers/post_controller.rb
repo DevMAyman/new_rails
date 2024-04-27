67489000de16b1fe9fe7c229746408708a338970
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-04-27 14:51:24 +0300

    create the app (Author 1:M Post) using scaffold

--- app/controllers/post_controller.rb
+++
@@ -1,8 +0,0 @@
-class PostController < ApplicationController
-    post = Post.create(title: "Mohamed", desc: "I am a man called MoAyman Saleh Mohamed")
-    all_posts = Post.all
-
-    all_posts.each do |post|
-      puts "Title: #{post.title}, Description: #{post.desc}"
-    end
-end
--- db/migrate/20240427113005_create_posts.rb
+++ db/migrate/20240427113005_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :desc
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
