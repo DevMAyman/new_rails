41e4de03a77e487afde05bc5dafc619592ece855
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-02 17:13:15 +0300

    create a creator association in the author, that links with the post as well (creator 1:M post)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  belongs_to :author
-  # belongs_to :creator, class_name: "Author"
+  # belongs_to :author
+  belongs_to :creator, class_name: "Author", foreign_key: "author_id" 
 end
